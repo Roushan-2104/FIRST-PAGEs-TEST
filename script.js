c156e762282a0b4644ff966eef9c4aa4692db10b
Roushan Sharma <87894817+Roushan-2104@users.noreply.github.com>
2021-08-16 23:53:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+/* -------FIRST SCRIPT------- */
+console.log("          <------------ NUMBER, BOOLEAN, STRINGS ------------->          ");
+//number, boolean, string
+var fir = 21.4 ,sec;
+console.log("FIRST NUMBER =>",fir);
+console.log(sec); //undefined value because no value is defined to it.
+
+/* -------TYPE OF DATA TYPES------- */
+console.log("-------- TYPE OF DATA TYPES ---------");
+var dob = "D.0.B => ";
+var char = "21 APRIL 2007";
+var num1 = 4.2007;
+console.log("BOOLEAN:true =>",typeof true); // boolean
+console.log("STRING:21 APRIL 2007 =>",typeof char); //string
+console.log("NUMBER:4.2007 =>", typeof num1); //number
+console.log(dob + ' ' +char); // will add both strings
+console.log("LENGTH OF (dob) VARIABLE =>",dob.length); 
